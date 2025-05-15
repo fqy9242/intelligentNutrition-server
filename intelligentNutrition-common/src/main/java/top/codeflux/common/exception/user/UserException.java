5f2f83e5238b530c504acbc59f8efea5c91c3cdf
qht <qht20050506@outlook.com>
2025-05-15 17:23:59 +0800

    init

---
+++ intelligentNutrition-common/src/main/java/top/codeflux/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package top.codeflux.common.exception.user;
+
+import top.codeflux.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
